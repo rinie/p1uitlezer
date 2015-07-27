f2e589c7151f2933718a1f7d6be37bdbb70e4a4f
Rinie Kervel <Rinie.Kervel@gmail.com>
2015-07-27 20:09:24 +0200

    Adapt speed to 115200 8n1 for DSM 4 protocol Make logging/export optional

--- p1uitlezer.js
+++ p1uitlezer.js
@@ -8,16 +8,22 @@ var currentDir = __dirname;
 var datestamp = "";
 var dataDir = "data";
 var writeStream = null;
+var logConsole = true;
+var logExport = false;
 
 var SerialPort = serialport.SerialPort; // localize object constructor
 var serialPort = new SerialPort("/dev/ttyUSB0", {
-  baudrate: 9600, 
-  databits:7, 
-  parity:"even", 
-  stopbits: 1, 
-  xon: 0, 
-  xoff: 0, 
+	/*
+  baudrate: 9600,
+  databits:7,
+  parity:"even",
+  stopbits: 1,
+  xon: 0,
+  xoff: 0,
   rtscts: 0,
+  */
+  // DSM 4 115200 8N1
+  baudrate: 115200,
   parser: serialport.parsers.readline("!")
 });
 
@@ -26,8 +32,10 @@ serialPort.on("open", function () {
   serialPort.on("data", function(data) {
     var exportData = [];
     var timestamp = moment().unix();
-    exportData.push(timestamp);
-    //console.log(data);
+    if (logExport) {
+    	exportData.push(timestamp);
+	}
+    console.log(data);
     var lines = data.split("\n");
     lines.forEach(function(line, i) {
       // get meter type
@@ -36,57 +44,95 @@ serialPort.on("open", function () {
       var meterValue = getStringBetween(line,"(","*");
       switch(meterType){
         case "1.8.1":
-          //console.log("afgenomen_hoog\t" + meterValue);
-          exportData.push(convertToIntegerValue(meterValue));
+		if (logConsole) {
+          console.log("afgenomen_hoog\t" + meterValue);
+	    }
+		if (logExport) {
+			  exportData.push(convertToIntegerValue(meterValue));
+		}
           break;
-        case "1.8.2": 
-          //console.log("afgenomen_laag\t" + meterValue);
-          exportData.push(convertToIntegerValue(meterValue));
+        case "1.8.2":
+		if (logConsole) {
+          console.log("afgenomen_laag\t" + meterValue);
+	    }
+		if (logExport) {
+			  exportData.push(convertToIntegerValue(meterValue));
+		}
           break;
         case "2.8.1":
-          //console.log("geleverd_hoog\t" + meterValue);
-          exportData.push(convertToIntegerValue(meterValue));
+		if (logConsole) {
+          console.log("geleverd_hoog\t" + meterValue);
+	    }
+		if (logExport) {
+			  exportData.push(convertToIntegerValue(meterValue));
+		}
           break;
         case "2.8.2":
-          //console.log("geleverd_laag\t" + meterValue);
-          exportData.push(convertToIntegerValue(meterValue));
+		if (logConsole) {
+          console.log("geleverd_laag\t" + meterValue);
+	    }
+		if (logExport) {
+			  exportData.push(convertToIntegerValue(meterValue));
+		}
           break;
         case "96.14.0":
           meterValue = "" + getStringBetween(line,"(",")");
-          //console.log("actueel_tarief\t" + (meterValue === "0001" ? "laag" : "hoog"));
-          exportData.push(meterValue === "0001" ? "l" : "h");
+		if (logConsole) {
+          console.log("actueel_tarief\t" + (meterValue === "0001" ? "laag" : "hoog"));
+	    }
+		if (logExport) {
+			  exportData.push(meterValue === "0001" ? "l" : "h");
+		}
           break;
         case "1.7.0":
-          //console.log("huidig_verbruik\t" + meterValue);
+		if (logConsole) {
+          console.log("huidig_verbruik\t" + meterValue);
+	    }
           // *10 to convert current consumption in W
-          exportData.push(convertToIntegerValue(meterValue)*10);
+		if (logExport) {
+			  exportData.push(convertToIntegerValue(meterValue)*10);
+		}
           break;
         case "2.7.0":
-          //console.log("huidig_levering\t" + meterValue);
+		if (logConsole) {
+          console.log("huidig_levering\t" + meterValue);
+	    }
           // *10 to convert current consumption in W
-          exportData.push(convertToIntegerValue(meterValue)*10);
+		if (logExport) {
+			  exportData.push(convertToIntegerValue(meterValue)*10);
+		}
           break;
         case "96.13.1":
           meterValue = getStringBetween(line,"(",")");
-          //console.log("bericht_num\t" + meterValue);
-          exportData.push(meterValue);
+		if (logConsole) {
+          console.log("bericht_num\t" + meterValue);
+	    }
+		if (logExport) {
+			  exportData.push(meterValue);
+		}
           break;
         case "96.13.0":
           meterValue = getStringBetween(line,"(",")");
-          //console.log("bericht_tekst\t" + meterValue);
-          exportData.push(meterValue);
+		if (logConsole) {
+          console.log("bericht_tekst\t" + meterValue);
+	    }
+		if (logExport) {
+			  exportData.push(meterValue);
+		}
           break;
         default:
           // unknown meter type
-          //console.log("unknown: " + line);
+		if (logConsole) {
+          console.log("unknown: " + line);
+	    }
       }
 
     });
-    
-    //console.log(exportData);
-    if (exportData.length === 10){
+
+    console.log(exportData);
+    if (logExport && (exportData.length === 10)){
         // export only valid p1 messages (where we could find all our wanted information)
-	// timestamp, 1.8.1, 1.8.2, 2.8.1, 2.8.2, 
+	// timestamp, 1.8.1, 1.8.2, 2.8.1, 2.8.2,
         var line = exportData.join(",");
         // TODO: write to rotated file
         var currentDatestamp = moment().format('YYMMDD');
